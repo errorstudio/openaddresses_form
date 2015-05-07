2368bdf13221a418b2d880809a1ab10997bd0bd1
Ed Jones <ed@errorstudio.co.uk>
2015-05-07 15:20:23 +0100

    embed WIP

--- embed.js
+++ embed.js
@@ -1,20 +1,40 @@
-var OpenAddresses = {};
+/*
+Configuration options:
+
+{
+ labelText: "Label for the field",
+ placeHolder: "Enter an address",
+
+}
+ */
+
 if(!console.log) console.log = function(){};
+var OpenAddresses = {
+    SortingOffice: function(config) {
+        _this = this;
+        configure = function() {
+            var iframe = document.createElement("iframe");
+            iframe.src="//localhost:8000/embed.html";
+            iframe.width = '100%';
+            //iframe.height = options.height || '650';
+            iframe.frameBorder = 0;
+            iframe.seamless = 'seamless';
+            document.getElementById("openaddresses-form").appendChild(iframe);
+        };
 
-OpenAddresses.SortingOffice = function() {
-    var iframe = document.createElement("iframe");
-    iframe.src="//localhost:8000/embed.html";
-    iframe.width = '100%';
-    //iframe.height = options.height || '650';
-    iframe.frameBorder = 0;
-    iframe.seamless = 'seamless';
-    document.getElementById("openaddresses-form").appendChild(iframe);
+        inject = function() {
+
+        }
+
+        OpenAddresses.config = config;
+        this.configure();
+        this.inject();
+    }
 };
+OpenAddress.SortingOffice({
 
+})
 
-OpenAddresses.SortingOffice.prototype.injectIframe = function() {
-
-};
 
 
 
